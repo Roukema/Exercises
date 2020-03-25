9c094e47d77edcdc6ee9f1a8552028cbaecf30b0
Roukema <58312286+Roukema@users.noreply.github.com>
2020-03-25 18:29:35 +0100

    list people from country still not sorted

--- Werken met grote arrays en objecten/script.js
+++ Werken met grote arrays en objecten/script.js
@@ -1,7 +1,15 @@
 //button on click
-// data ophalen // hoeft niet
 // data sorteren
 // data to dom
+const year = new Date()
+  .getFullYear()
+  .toString()
+  .substr(-2);
+const month = new Date().getMonth() + 1;
+
+const emptyDOM = () => {
+  document.getElementById("lijst").innerText = "";
+};
 
 const addDataToList = data => {
   let listnode = document.createElement("li");
@@ -22,14 +30,14 @@ const landenLijstToDom = () => {
     addDataToList(element);
   });
 };
-document
-  .getElementById("landenLijst")
-  .addEventListener("click", () => landenLijstToDom());
+document.getElementById("landenLijst").addEventListener("click", () => {
+  emptyDOM();
+  landenLijstToDom();
+});
 
 // Steenbokken
 
 const addPeopleList = data => {
-  console.log(data);
   let listnode = document.createElement("li");
   let divnode = document.createElement("div");
   let imgnode = document.createElement("img");
@@ -56,15 +64,14 @@ const sortDataName = () => {
     return 0;
   });
   return data;
-  //   filterWomen(data);
 };
 
 const filterWomen = data => {
   women = data.filter(person => person.gender === "female");
   return women;
 };
-const filterAge = data => {
-  ageFilter = data.filter(person => person.age > 30);
+const filterAge = (data, leeftijd) => {
+  ageFilter = data.filter(person => person.age > leeftijd);
   return ageFilter;
 };
 const filterBirthday = data => {
@@ -73,15 +80,95 @@ const filterBirthday = data => {
     return (day > 22 && month == 12) || (day < 19 && month === 1);
   });
 
-  console.log(birthdayfilter);
   return birthdayfilter;
 };
 
 const peopleToDom = () => {
-  filterBirthday(filterAge(filterWomen(sortDataName()))).forEach(element => {
-    addPeopleList(element);
-  });
+  filterBirthday(filterAge(filterWomen(sortDataName()), 30)).forEach(
+    element => {
+      addPeopleList(element);
+    }
+  );
 };
-document
-  .getElementById("steenbok")
-  .addEventListener("click", () => peopleToDom());
+
+document.getElementById("steenbok").addEventListener("click", () => {
+  emptyDOM();
+  peopleToDom();
+});
+
+// oude creditcards
+
+const addOldCreditcarsList = data => {
+  let listnode = document.createElement("li");
+  let divnode = document.createElement("div");
+  let namenode = document.createElement("p");
+  let numbernode = document.createElement("p");
+  let cardnode = document.createElement("p");
+  let experationnode = document.createElement("p");
+
+  namenode.innerText = data.name + " " + data.surname;
+  numbernode.innerText = data.phone;
+  cardnode.innerText = data.credit_card.number;
+  experationnode.innerText = data.credit_card.expiration;
+
+  divnode.appendChild(namenode);
+  divnode.appendChild(numbernode);
+  divnode.appendChild(cardnode);
+  divnode.appendChild(experationnode);
+  document
+    .getElementById("lijst")
+    .appendChild(listnode)
+    .appendChild(divnode);
+};
+const filterExperation = data => {
+  expiration = data.filter(person => {
+    let [expMonth, expYear] = person.credit_card.expiration.split("/");
+    return (expMonth > month && expYear == year) || expYear > year;
+  });
+
+  return expiration;
+};
+const sortExpDate = data => {
+  data.sort(function(a, b) {
+    let [monthA, yearA] = a.credit_card.expiration.split("/"),
+      [monthB, yearB] = b.credit_card.expiration.split("/");
+    return yearA - yearB || monthA - monthB;
+  });
+  return data;
+};
+
+const checkCreditCards = () =>
+  sortExpDate(filterExperation(filterAge(randomPersonData, 18))).map(
+    addOldCreditcarsList
+  );
+document.getElementById("credit").addEventListener("click", () => {
+  emptyDOM();
+  checkCreditCards();
+});
+
+// teller landen
+
+const addCountToList = data => {
+  let listnode = document.createElement("li");
+  let count = 0;
+  landenlijst = data;
+
+  randomPersonData.forEach(element => {
+    landenlijst.includes(element.region) ? count++ : count;
+  });
+
+  listnode.innerText = data + " " + count;
+  document.getElementById("lijst").appendChild(listnode);
+};
+
+// const addCounter = data => {
+//   let count = 0;
+//   landenlijst = data;
+//   randomPersonData.forEach(element => {
+//     landenlijst.includes(element.region)
+//       ? { land: element.region, count: count++ }
+//       : { land: element.region, count: count };
+//   });
+// };
+
+filterUniek(filterLanden()).forEach(addCountToList);
